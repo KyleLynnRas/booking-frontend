df19a2143ee3807ed9a00798def9ba2cc05bb01c
KyleLynnRas <kylelynn.ras@gmail.com>
2021-06-29 14:11:18 -0400

    Show auth working

--- src/pages/performances/Show.js
+++ src/pages/performances/Show.js
@@ -5,16 +5,25 @@ import Review from "../../components/Review";
 import Form from "../../components/Form";
 
 const Show = (props) => {
+	useEffect(() => {
+		console.log(props.users.token);
+		if (props.users.token) {
+		} else {
+			props.history.push("/login");
+		}
+	}, []);
+
 	//performance id from params
 	const id = props.match.params.id;
 	const performances = props.performances;
-	const performance = performances.find((p) => p.id == id);
 
 	const loading = () => {
 		return <h1>Loading....</h1>;
 	};
 
 	const loaded = () => {
+		//find performance
+		const performance = performances.find((p) => p.id == id);
 		//return related reviews into new array
 		const revArr = props.reviews.filter((ele) => {
 			if (ele.performance.id == id) {
@@ -22,18 +31,36 @@ const Show = (props) => {
 			}
 		});
 
-		return revArr.map((ele) => {
-			return (
-				<Review
-					key={ele.id}
-					id={ele.id}
-					category={ele.category}
-					content={ele.content}
-					author={ele.user.username}
-					destroyReview={props.destroyReview}
-				/>
-			);
-		});
+		return (
+			<div>
+				<section>
+					<img src={performance.img} alt={performance.title} />
+					<p>{performance.summary}</p>
+					<p>$ {performance.price}</p>
+					<a
+						href="mailto:peculiarityproductions@gmail.com?Subject=Quote%20request"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<button>Request a booking</button>
+					</a>
+				</section>
+				<section>
+					<h1>Reviews</h1>
+					{revArr.map((ele) => (
+						<Review
+							key={ele.id}
+							id={ele.id}
+							category={ele.category}
+							content={ele.content}
+							author={ele.user.username}
+							destroyReview={props.destroyReview}
+						/>
+					))}
+					;
+				</section>
+			</div>
+		);
 	};
 
 	return (
@@ -41,22 +68,7 @@ const Show = (props) => {
 			<header>
 				<h1>{performance.title}</h1>
 			</header>
-			<section>
-				<img src={performance.img} alt={performance.title} />
-				<p>{performance.summary}</p>
-				<p>$ {performance.price}</p>
-				<a
-					href="mailto:peculiarityproductions@gmail.com?Subject=Quote%20request"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<button>Request a booking</button>
-				</a>
-			</section>
-			<section>
-				<h1>Reviews</h1>
-				{props.reviews ? loaded() : loading()}
-			</section>
+			{props.reviews ? loaded() : loading()}
 			<section>
 				<h3>Add Review</h3>
 				<Form users={props.users} submitFunc={props.create} id={id} />
